d0b2c29bd07c392eac607b4ee9639592f152183a
佛系玩家沈公子 <techpang666@gmail.com>
2021-03-04 07:52:16 +0800

    pass_Object_keys_log_arr_obj

---
+++ code/obj/pass_Object_keys_log_arr_obj.js
@@ -0,0 +1,9 @@
+// 通过Object.keys()打印数组或者对象
+
+const arr = ['a', 'b', 'c'];
+// 打印数组的索引
+console.log(Object.keys(arr));
+
+const obj = { 0: 'a', 1: 'b', 2: 'c' };
+// 打印对象的属性名
+console.log(Object.keys(obj));
