44243750abb2ad806f539058f51cc7368d8b0f3a
佛系玩家沈公子 <techpang666@gmail.com>
2021-03-08 03:02:45 +0800

    pass_Set_arr_repeat

---
+++ code/arr/arr_repeat.js
@@ -0,0 +1,9 @@
+
+// 准备数组及打印一下长度
+const arr = [1, 8, 5, 6, 4, 1, 0, 4, 7, 0, 7, 7, 8, 9, 9]
+console.log(arr.length)
+
+// 通过ES6的Set去重
+const res = new Set(arr)
+console.log(res.size)
+console.log(res)
