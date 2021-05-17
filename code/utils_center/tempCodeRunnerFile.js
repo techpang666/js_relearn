83ebfc15a7658353e463785b55c77b34c50a3b74
佛系玩家沈公子 <techpang666@gmail.com>
2021-05-17 17:43:43 +0800

    time

---
+++ code/utils_center/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+
+let time = new Date()
+let month = time.getMonth() + 1
+let date = time.getDate()
+console.log(month + '.' + date);
---
+++ code/utils_center/time.js
@@ -0,0 +1,9 @@
+
+/**
+ * @description 随便写个时间工具
+ */
+
+let time = new Date()
+let month = time.getMonth() + 1
+let date = time.getDate()
+console.log(month + '.' + date);
