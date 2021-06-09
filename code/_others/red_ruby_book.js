194f3083c573dd34dfede7818301539e2431d3a1
沈公子 <techpang666@gmail.com>
2021-06-10 05:38:29 +0800

    js210610

--- code/_others/red_ruby_book.js
+++ code/_others/red_ruby_book.js
@@ -905,3 +905,11 @@ let num = 10
 console.log(num.toString()); /* 10 */
 console.log(num.toString(2)); /* 1010 */
 console.log(num.toString(8)); /* 12 */
+
+/**
+ * @todo 210610
+ */
+
+/**
+ * @description 可能停服个几天
+ */
