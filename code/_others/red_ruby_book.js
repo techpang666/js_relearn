96a419087d18b66b61cae5bb01741a60739d8005
沈公子 <techpang666@gmail.com>
2021-06-14 23:16:33 +0800

    fix210613

--- code/_others/red_ruby_book.js
+++ code/_others/red_ruby_book.js
@@ -1013,3 +1013,23 @@ append() /* testtesttest */
  * @todo 210613
  */
 
+/**
+ * @description 模板字面量标签函数
+ */
+
+let a = 6
+let b = 9
+
+function simpleTag(strings, a, b, sum) {
+	console.log(strings);
+	console.log(a);
+	console.log(b);
+	console.log(sum);
+	return 'testStr'
+}
+
+let res = `${a} + ${b} = ${a + b}`
+let tagRes = simpleTag`${a} + ${b} = ${a + b}`
+
+console.log(res);
+console.log(tagRes);
--- code/_others/tempCodeRunnerFile.js
+++ code/_others/tempCodeRunnerFile.js
@@ -1,4 +1,17 @@
 
-let value = 5
-let testTemplate = `test: ${value}`
-console.log(testTemplate); /* hello, world */
+let a = 6
+let b = 9
+
+function simpleTag(strings, a, b, sum) {
+	console.log(strings);
+	console.log(a);
+	console.log(b);
+	console.log(sum);
+	return 'testStr'
+}
+
+let res = `${a} + ${b} = ${a + b}`
+let tagRes = simpleTag`${a} + ${b} = ${a + b}`
+
+console.log(res);
+console.log(tagRes);
