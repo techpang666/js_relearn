d1b28680b5071535eedb1dcb8752bd4f76ed9c6e
沈公子 <techpang666@gmail.com>
2021-06-25 18:40:06 +0800

    destructuring

---
+++ code/destructuring/destructuring.js
@@ -0,0 +1,25 @@
+
+/**
+ * @description 解构
+ */
+
+/**
+ * @description 数组解构
+ */
+
+// 左右配对模式
+
+let [demo, [[test], name]] = [1, [[2, 3], 4]]
+console.log(test); /* 2 */
+
+let [demo, ...detail] = [1, 2, 3, 4]
+console.log(detail); /* [ 2, 3, 4 ] */
+
+let [x, y, ...z] = ['a']
+console.log(y); /* undefined */
+console.log(z); /* [] */
+
+// 解构不成功 会undefined
+
+let [demo] = []
+console.log(demo); /* undefined */
---
+++ code/destructuring/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+
+let [demo] = []
+console.log(demo);
